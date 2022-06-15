ed0119c0334c8cbba5a045b2eb1f5660eba39749
Dixitgupta222 <Dixitgupta222@gmail.com>
2022-06-15 13:39:46 +0530

    Content Updated2.4

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
           sm: '600px',
           md: '728px',
           lg: '984px',
-          xl: '1240px',
+          xl: '1240px', 
           '2xl': '1450px',
         },
       },
